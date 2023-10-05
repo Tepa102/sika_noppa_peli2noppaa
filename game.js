f8197d214e4e20107ec112bbcc0397d691e3f75a
Terttu Toivonen <terttu.toivonen@esedulainen.fi>
2023-10-05 14:19:39 +0300

    piste lasku ei vain toimi

--- game.js
+++ game.js
@@ -65,7 +65,8 @@ function rollDice() {
     if (roll1 === 1 && roll2 === 1) {
         // Jos heittää 1 ja 1, lisää 25 pistettä
         currentTurnScore += 25;
-        resultElement.textContent = `${players[currentPlayer].name} heitti 1 ja 1 ja sai 25 pistettä ja vuoron pisteet ovat nyt ${currentTurnScore}.`;
+        resultElement.textContent = `${players[currentPlayer].name} heitti 1 ja 1 ja sai 25 pistettä 
+        ja vuoron pisteet ovat nyt ${currentTurnScore}.`;
     } else if (roll1 === roll2 && roll1 === 1) {
         // Jos heittää kolme peräkkäistä tuplaheittoa, menettää vuoron ja pisteet
         currentTurnScore = 0;
@@ -78,7 +79,8 @@ function rollDice() {
         endTurn();
     } else if (roll1 === roll2) {
         currentTurnScore = (roll1 + roll2)*2; 
-        resultElement.textContent = `${players[currentPlayer].name} heitti ${roll1} ja ${roll2} ja sai ${currentTurnScore} ja vuoron pisteet ovat nyt ${currentTurnScore}.`;
+        resultElement.textContent = `${players[currentPlayer].name} heitti ${roll1} ja ${roll2} ja sai ${currentTurnScore} 
+        ja vuoron pisteet ovat nyt ${currentTurnScore}.`;
     } else {
         currentTurnScore += roll1 + roll2;
         resultElement.textContent = `${players[currentPlayer].name} heitti ${roll1} ja ${roll2},
